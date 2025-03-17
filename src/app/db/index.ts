571a210cc6fd8eca7704916caae78eade0d402df
Alexandre Duteau <alex@midaslabs.co>
2025-03-16 21:04:40 -0600

    Just commented out the schema so i could deply

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/libsql';
-import * as schema from "./schema";
+// import * as schema from "./schema";
 config({ path: '.env' }); // or .env.local
 
 export const db = drizzle({ connection: {
